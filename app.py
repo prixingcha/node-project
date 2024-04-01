46e73094cf4db88e865ec79ed3658983ae3384c0
prixingcha <pratik.np@gmail.com>
2024-04-01 19:31:56 -0400

    added ollama files in node project.

---
+++ app.py
@@ -0,0 +1,19 @@
+import localImport
+
+print(f"OPENAI_API_KEY       : {localImport.OPENAI_API_KEY       }")
+print(f"OPENAI_MODEL_NAME    : {localImport.OPENAI_MODEL_NAME    }") 
+print(f"OPENAI_API_BASE      : {localImport.OPENAI_API_BASE      }")
+print(f"SERPER_API_KEY       : {localImport.SERPER_API_KEY       }") 
+print(f"POLYGON_API_KEY      : {localImport.POLYGON_API_KEY      }")
+print(f"GOOGLE_MAPS_API_KEY  : {localImport.GOOGLE_MAPS_API_KEY  }")          
+print(f"WORKSPACE_DIR: {localImport.WORKSPACE_DIR}")          
+
+
+
+OPENAI_API_KEY     = localImport.OPENAI_API_KEY     
+OPENAI_MODEL_NAME  = localImport.OPENAI_MODEL_NAME  
+OPENAI_API_BASE    = localImport.OPENAI_API_BASE    
+SERPER_API_KEY     = localImport.SERPER_API_KEY     
+POLYGON_API_KEY    = localImport.POLYGON_API_KEY    
+GOOGLE_MAPS_API_KEY= localImport.GOOGLE_MAPS_API_KEY
+WORKSPACE_DIR= localImport.WORKSPACE_DIR
---
+++ localImport.py
@@ -0,0 +1,5 @@
+from dotenv import load_dotenv
+import os
+import sys
+sys.path.append('/Users/pratiksingh/repo/projects/script-project/')
+from api_keys_project import OPENAI_API_KEY, OPENAI_MODEL_NAME, OPENAI_API_BASE, SERPER_API_KEY, POLYGON_API_KEY, GOOGLE_MAPS_API_KEY, WORKSPACE_DIR
